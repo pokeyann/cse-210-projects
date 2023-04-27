cb6f269e70b5a3dc48b286d2a1b2a1a7d9adce1b
pokeyann <pokeyann@gmail.com>
2023-04-27 12:16:51 -0500

    trouble debugging, but code done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,86 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep2 World!");
+
+    Console.WriteLine("What is your grade percentage? ");
+    string letterGrade = Console.ReadLine();
+
+
+    int a = int.Parse(letterGrade);
+
+    if (a >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.WriteLine("Your grade is an A.");
     }
+
+    else if (a >= 80)
+    {
+      Console.WriteLine("Your grade is a B.");
+    }
+
+    else if (a >= 70)
+    {
+      Console.WriteLine("Your grade is a C.");
+    }
+
+    else if (a >= 60)
+    {
+      Console.WriteLine("Your grade is a D.");
+    }
+
+    else
+    {
+      Console.WriteLine("Your grade is an F.");
+    }
+
+    if (a >= 70)
+    {
+      Console.WriteLine("Congratulations you passed the course!");
+    }
+
+    else
+    {
+      Console.WriteLine("Please try again.");
+    }
+
+    /*  Core Requirement 3, having issues debugging so can't check above code.  Writing this code so that I can check both, 
+    once I have the debugging issue fixed.
+    
+    Console.WriteLine("What is your grade percentage? ");
+    string letterGrade = Console.ReadLine();
+    int a = int.Parse(letterGrade);
+
+    string letter = "";
+    
+    if (a >= 90)
+    {
+      letter = "A";
+    }
+
+    else if (a >= 80)
+    {
+     letter = "B";
+    }
+
+    else if (a >= 70)
+    {
+     letter = "C";
+    }
+
+    else if (a >= 60)
+    {
+      letter = "D";
+    }
+
+    else
+    {
+     letter = "F";
+    }
+    
+    Console.WriteLine($"Your grade is {letter}.") */
+
+  }
 }
