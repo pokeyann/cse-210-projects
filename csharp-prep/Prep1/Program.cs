a2f60c7a48597a936890d69bd17283df3d25c0bb
pokeyann <pokeyann@gmail.com>
2023-04-27 10:06:04 -0500

    need to push launch json prep 1 ??

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.Write("What is your first name? ");
+    string fname = Console.ReadLine();
+
+
+    Console.Write("What is your last name? ");
+    string lname = Console.ReadLine();
+
+
+    Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+
+    /*Console.Write("What is your first name? ");
+    string first = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    string last = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {last}, {first} {last}.");*/
+
+  }
 }
+
+
+
