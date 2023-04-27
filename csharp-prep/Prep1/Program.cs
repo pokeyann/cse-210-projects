462ff29491dfa1a5ed6194ba7986190a5455f6af
pokeyann <pokeyann@gmail.com>
2023-04-27 11:45:15 -0500

    working program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -14,14 +14,6 @@ class Program
 
     Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
 
-    /*Console.Write("What is your first name? ");
-    string first = Console.ReadLine();
-
-    Console.Write("What is your last name? ");
-    string last = Console.ReadLine();
-
-    Console.WriteLine($"Your name is {last}, {first} {last}.");*/
-
   }
 }
 
