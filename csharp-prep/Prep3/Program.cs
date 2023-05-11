6424d6567c02fb803b408a41ac381b670d43e623
pokeyann <pokeyann@gmail.com>
2023-05-10 23:42:15 -0500

    path and debug issues fixed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,33 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep3 World!");
+
+    Console.WriteLine("What is the magic number? ");
+    string magicNumber = Console.ReadLine();
+
+    Console.WriteLine("What is your guess? ");
+    string guess = Console.ReadLine();
+
+    int a = int.Parse(magicNumber);
+    int b = int.Parse(guess);
+
+    if (a > b)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Console.WriteLine("Higher");
     }
+
+    else if (a < b)
+    {
+      Console.WriteLine("Lower");
+    }
+
+    else
+    {
+      Console.WriteLine("You guessed it!");
+    }
+
+  }
 }
