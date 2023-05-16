c526dae566e0bda8a2e94f781187e858eab98fc8
pokeyann <pokeyann@gmail.com>
2023-05-15 21:08:10 -0500

    list created

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep4 World!");
+
+    Console.WriteLine("Please enter a list of numbers, type 0 when finished.");
+
+    List<int> numbers = new List<int>();
+
+    int userNumber = -1;
+
+    while (userNumber != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+      Console.Write("Enter number: ");
+
+      string userResponse = Console.ReadLine();
+      userNumber = int.Parse(userResponse);
     }
+
+  }
 }
