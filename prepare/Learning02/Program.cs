3550616c65c5834d714230606612254ddaf737ae
pokeyann <pokeyann@gmail.com>
2023-05-23 17:30:28 -0500

    Create Job Class, test, add display

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Learning02 World!");
+
+
+    Job job1 = new Job();
+    job1._company = "Googly Eyes";
+    job1._jobTitle = "Placement Prankster";
+    job1._startYear = 1976;
+    job1._endYear = 2020;
+
+    Job job2 = new Job();
+    job2._company = "House o' Crackers";
+    job2._jobTitle = "Cracker Stacker Attacker";
+    job2._startYear = 2050;
+    job2._endYear = 2060;
+
+    job1.DisplayJobDetails();
+    job2.DisplayJobDetails();
+  }
 }
